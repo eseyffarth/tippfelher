8fd372b176a3c1eefa184cf486fce774c14bb846
Matthias Krüger <matthias.krueger@famsik.de>
2015-03-19 14:42:14 +0100

    refactor wrong_key, use keyboard-layout based matrix instead of sets for each letter.

--- main.py
+++ main.py
@@ -33,62 +33,39 @@ def miss_shift(text):
     return out_text
 
 
+def get_nearby_char(char):
+
+	# german keyboard layout
+	layout=['^', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'ß', '`'], \
+	            ['q', 'w', 'e', 'r', 't', 'z', 'u', 'i', 'o', 'p', 'ü', '*'], \
+	            ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'ö', 'ä', '#'], \
+	            ['<', 'y', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '-'], \
+	            [ '',  '', '',  ' ', ' ', ' ', ' ', ' ',  '',  '',  '']
+
+	if ((not char) or (not any(char in row for row in layout))): # no char or not found in layout
+		return char
+
+	for ix, row in enumerate(layout): # get index of char we want to manipulate
+		for iy, i in enumerate(row):
+			if i == char: # introduce the "typo", shift the index randomly by +-1
+				if i == ' ': # spacebar ...
+					iy += random.randint(-1, +5) # ... is a bit bigger
+				else:
+					iy += random.randint(-1, +1)
+				ix += random.randint(-1, +1)
+				ix = max(min(len(layout)-1, ix), 0) # prevent out of bounds
+				iy = max(min(len(layout[ix])-1, iy), 0)
+				return (layout[ix][iy])
+
 def wrong_key(text):
-    adjacent_keys = {'1':set(['2','q']),
-                    '2':set(['1','q','w','3']),
-                    '3':set(['2','w','e','4']),
-                    '4':set(['3','e','r','5']),
-                    '5':set(['4','r','t','6']),
-                    '6':set(['5','t','z','7']),
-                    '7':set(['6','z','u','8']),
-                    '8':set(['7','u','i','9']),
-                    '9':set(['8','i','o','0']),
-                    '0':set(['9','o','p','ß']),
-                    'ß':set(['0','ü']),
-                    'q':set(['1','2','w','a']),
-                    'w':set(['q','2','3','e','s','a']),
-                    'e':set(['w','3','4','r','d','s']),
-                    'r':set(['e','4','5','t','f','d']),
-                    't':set(['r','5','6','z','g','f']),
-                    'z':set(['t','6','7','u','h','g']),
-                    'u':set(['z','7','8','i','j','h']),
-                    'i':set(['u','8','9','o','k','j']),
-                    'o':set(['i','9','0','p','l','k']),
-                    'p':set(['o','0','ß','ü','ö','l']),
-                    'ü':set(['p','ß','+','ä','ö']),
-                    'a':set(['q','w','s','y','<']),
-                    's':set(['a','w','e','d','x','y']),
-                    'd':set(['s','e','r','f','c','x']),
-                    'f':set(['d','r','t','g','v','c']),
-                    'g':set(['f','t','z','h','b','v']),
-                    'h':set(['g','z','u','j','n',]),
-                    'j':set(['h','u','i','k','m','n']),
-                    'k':set(['j','i','o','l',',','m']),
-                    'l':set(['k','o','p','ö','.',',']),
-                    'ö':set(['l','p','ü','ä','-','.']),
-                    'ä':set(['ö','ü','+','#','-']),
-                    'y':set(['<','a','s','x']),
-                    'x':set(['y','s','d','c',' ']),
-                    'c':set(['x','d','f','v',' ']),
-                    'v':set(['c','f','g','b',' ']),
-                    'b':set(['v','g','h','n',' ']),
-                    'n':set(['b','h','j','m',' ']),
-                    'm':set(['n','j','k',',',' ']),
-                    ',':set(['m','k','l','.',' ']),
-                    '.':set([',','l','ö','-']),
-                    '-':set(['.','ö','ä']),
-                    ' ':set(['x','c','v','b','n','m',','])}
+
     out_text = ''
     for letter in text:
         choice = random.randint(1,61)
-        if choice <= 2:                     # 2 in 60 characters will be replaced with an adjacent key (only lower-case)
-            if letter in adjacent_keys:
-                out_text += random.sample(adjacent_keys[letter],1)[0]
-            else:
-                out_text += letter
+        if choice <= 2:                     # 2 in 60 characters will be replaced with an adjacent letter (only lower-case)
+            out_text += get_nearby_char(letter)
         elif choice == 4:                   # 1 in 60 characters will be followed by an adjacent letter
-            if letter in adjacent_keys:
-                out_text += letter + random.sample(adjacent_keys[letter],1)[0]
+            out_text += letter + get_nearby_char(letter)
         else:
             out_text += letter
 
